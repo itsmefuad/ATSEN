1b0f8492b1259815d109fef5db285d6e5dd5e799
Fuad Fardin <fuadfardin214@gmail.com>
2025-08-10 22:10:04 +0600

    optimizing files and folder structure

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+//prefixing
+app.use("/api/notes", notesRoutes);
+
+
+app.listen(5001, () => {
+    console.log("Server started on PORT: 5001");
+});
