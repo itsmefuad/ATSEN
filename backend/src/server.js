ea4c0580e2b169e36f7cfb289327b15b5ff44b8d
Fuad Fardin <fuadfardin214@gmail.com>
2025-08-17 01:59:16 +0600

    added uploading materials funtionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import roomsRoutes from "./routes/roomsRoutes.js";
 import institutionRoute from "./routes/institutionRoutes.js";      // ← import it
 import announcementRoutes from "./routes/announcementRoutes.js";
+import materialRoutes from "./routes/materialRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use("/api/rooms", roomsRoutes);
 app.use("/api/institutions", institutionRoute);               // ← mount it
 app.use("/api/announcements", announcementRoutes);
+app.use("/api/materials", materialRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
