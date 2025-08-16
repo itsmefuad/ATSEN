2a685a66076a3bd8bbfd24f37629f31f0b5abb3c
Fuad Fardin <fuadfardin214@gmail.com>
2025-08-17 02:05:31 +0600

    undid some changes

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,19 +1,13 @@
-// Temporarily disabled Upstash Redis for development
-// import { Ratelimit } from "@upstash/ratelimit";
-// import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const ratelimit = new Ratelimit({
-//   redis: Redis.fromEnv(),
-//   limiter: Ratelimit.slidingWindow(100, "10 s"), // 5 requests every 10 seconds
-// });
-
-// Temporary mock rate limiter that always allows requests
-const ratelimit = {
-  limit: async () => ({ success: true })
-};
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(100, "10 s"), // 5 requests every 10 seconds
+});
 
 export default ratelimit;
